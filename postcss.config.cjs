d29f2824a03d22a0ca494ed86fd5f885eca8b105
Xavier Egea <xavier.egea@gmail.com>
2025-11-02 12:12:10 +0100

    chore: install Tailwind and configure PostCSS; include styles in tailwind content

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // The PostCSS plugin for Tailwind has moved to a separate package
+    // https://tailwindcss.com/docs/installation
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
