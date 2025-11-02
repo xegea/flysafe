222512ace9d19b8390fbdf2c64bce8352b204f43
xegea <xegea@users.noreply.github.com>
2025-11-02 12:16:27 +0000

    deploy: 2298a0a3e780eddfcb7a934f17de41a17ab002fe

--- _next/static/chunks/a4ddb7094a62c26b.js
+++ _next/static/chunks/a4ddb7094a62c26b.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/650f8e5c8be6322b.js",
   "static/chunks/85eb15a5b140de35.js",
   "static/chunks/965c3de97e6a1e4a.js",
-  "static/chunks/018f705ce4983d81.css",
-  "static/chunks/turbopack-df3f6cb27ce41976.js"
+  "static/chunks/5649eae878f6b217.css",
+  "static/chunks/turbopack-66156b4c8e6b1335.js"
 ])
